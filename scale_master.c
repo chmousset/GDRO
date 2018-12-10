5efaa1aeda30cdaa3bf326c0136d59efabb2d22c
chmousset <ch.mousset@gmail.com>
2018-12-11 00:36:53 +0100

    [fix] updated license and readme

--- scale_master.c
+++ scale_master.c
@@ -1,5 +1,16 @@
-// Copyright 2018 Charles-Henri Mousset
 // Driver for scales and calipers that need an external clock
+// Copyright (C) 2018 Charles-Henri Mousset - ch.mousset@gmail.com
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "ch.h"
 #include "hal.h"
